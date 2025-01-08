658edf1db27b1b4c10a63239da4c6fc74f828473
Ozne <xxtagashixx@gmail.com>
2025-01-08 16:22:51 +0100

    :fix api baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -2,20 +2,4 @@ import axios from "axios";
 
 const api = axios.create({ baseURL: "http://192.168.1.70:8000" });
 
-// api.interceptors.request.use(
-//   async (config) => {
-//     const jsonValue = await AsyncStorage.getItem("token");
-//     if (jsonValue !== null) {
-//       const token = JSON.parse(jsonValue);
-//       if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//       }
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
 export { api };
